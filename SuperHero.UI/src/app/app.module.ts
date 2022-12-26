e78e6d2d3664d0222d74643ff83a6c08bcb1b186
AGallo <alejandro.gallo@globalprocessing.com.ar>
2022-12-26 02:09:12 -0300

    Fin del desarrollo con bootstrap

--- SuperHero.UI/src/app/app.module.ts
+++ SuperHero.UI/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EditHeroComponent } from './components/edit-hero/edit-hero.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
@@ -16,7 +19,8 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
